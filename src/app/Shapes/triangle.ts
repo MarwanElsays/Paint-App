80ede5e4462a73d30ab049f6cbc35bbed23d85e6
Ali M <AliIsHomie@outlook.com>
2022-12-08 16:39:09 +0200

    fixed indentation

--- src/app/Shapes/triangle.ts
+++ src/app/Shapes/triangle.ts
@@ -1,29 +1,29 @@
 import { Shape } from './shape';
 
 export class Triangle extends Shape{
-    override Draw(ctx: CanvasRenderingContext2D, color: string, x: number, y: number, startx: number, starty: number) {
-        ctx.strokeStyle = color;
-        ctx.moveTo(startx, starty);
-        ctx.lineTo(x, y);
-        ctx.lineTo(startx - (x - startx), y);
-        ctx.closePath();
-        ctx.stroke();
-    
-        this.w = (x - startx) * 2;
-        this.h = y - starty;
-        this.x = startx - this.w / 2;
-        this.y = starty;
-        this.col = color;
-        this.valid = true;
-      }
+  override Draw(ctx: CanvasRenderingContext2D, color: string, x: number, y: number, startx: number, starty: number) {
+    ctx.strokeStyle = color;
+    ctx.moveTo(startx, starty);
+    ctx.lineTo(x, y);
+    ctx.lineTo(startx - (x - startx), y);
+    ctx.closePath();
+    ctx.stroke();
 
-      override Update(ctx: CanvasRenderingContext2D) {
-        ctx.strokeStyle = this.col;
-        ctx.beginPath();
-        ctx.moveTo(this.x + this.w / 2, this.y);
-        ctx.lineTo(this.x + this.w, this.y + this.h);
-        ctx.lineTo(this.x, this.y + this.h);
-        ctx.closePath();
-        ctx.stroke();
-      }
+    this.w = (x - startx) * 2;
+    this.h = y - starty;
+    this.x = startx - this.w / 2;
+    this.y = starty;
+    this.col = color;
+    this.valid = true;
+  }
+
+  override Update(ctx: CanvasRenderingContext2D) {
+    ctx.strokeStyle = this.col;
+    ctx.beginPath();
+    ctx.moveTo(this.x + this.w / 2, this.y);
+    ctx.lineTo(this.x + this.w, this.y + this.h);
+    ctx.lineTo(this.x, this.y + this.h);
+    ctx.closePath();
+    ctx.stroke();
+  }
 }
